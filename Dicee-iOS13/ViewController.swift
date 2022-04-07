fa6cdc4c22bd22ef45ff64131256bf06d92d5428
Nanhe Maurya <info.nanhemaurya@gmail.com>
2022-04-07 10:56:00 +0530

    [+] Comments in VC & AppDelegate

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+//        ghhjgj jgjh gjhg jhg jhg jhg jhg jh
+//        kjhkjhhjkhh kj hkjh kjhjk
+        
         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
         diceImageView1.image = diceArray[Int.random(in: 0...5)]
